0cd7fe742a1745e3e832bdf1795abc7db94a2bb8
marivaz0 <118058661+marivaz0@users.noreply.github.com>
2023-03-10 13:55:03 -0300

    Update Data.java

--- aula4/ex2/Data.java
+++ aula4/ex2/Data.java
@@ -1,5 +1,8 @@
 package edu.br.marielle.aula4.ex2;
 
+/* Escreva um construtor para uma classe Data que receba os valores correspondentes ao dia, m√™s e ano, 
+e inicialize os campos da classe, verificando antes se a data √© v√°lida. (dias entre 1 e 31, meses entre 1 e 12 e anos maiores que 0).*/
+
 public class Data {
 	private int dia, mes, ano;
 	
@@ -7,17 +10,17 @@ public class Data {
 		if((dia >= 1) && (dia <= 31)) {
 			this.dia = dia;
 		} else {
-			System.out.println("Dia inv·lido!");
+			System.out.println("Dia inv√°lido!");
 		}
 		if((mes >= 1) && (mes <= 12)) {
 			this.mes = mes;
 		} else {
-			System.out.println("MÍs inv·lido!");
+			System.out.println("M√™s inv√°lido!");
 		}
 		if(ano > 0) {
 			this.ano = ano;
 		} else {
-			System.out.println("Ano inv·lido!");
+			System.out.println("Ano inv√°lido!");
 		}
 	}
 
