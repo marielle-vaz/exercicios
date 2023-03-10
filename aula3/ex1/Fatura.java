ce47eb069d862448e065d6fe39ffaf08939b7ed5
marivaz0 <118058661+marivaz0@users.noreply.github.com>
2023-03-10 13:49:50 -0300

    Update Fatura.java

--- aula3/ex1/Fatura.java
+++ aula3/ex1/Fatura.java
@@ -1,5 +1,12 @@
 package edu.br.marielle.aula3.ex1;
 
+/*Crie uma classe em Java chamada Fatura para uma loja de suprimentos de informática. A classe deve conter quatro variáveis – o número (String), 
+a descrição (String), a quantidade comprada de um item (int) e o preço por item (double).
+A classe deve ter um construtor e um método get e set para cada variável de instância.
+Além disso, forneça um método chamado getTotalFatura que calcula o valor da fatura e depois retorna o valor como um double. 
+Se o valor não for positivo, ele deve ser configurado como 0. Se o preço por item não for positivo, ele deve ser configurado como 0.0.
+Escreva uma classe de teste chamada FaturaTeste (em outro arquivo) que demonstra as capacidades da classe Fatura.*/
+
 public class Fatura {
 	
 	private String numero, descricao;
