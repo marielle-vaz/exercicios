55ec529a232fd55fb08be7c28a27db5f4d19f078
marivaz0 <118058661+marivaz0@users.noreply.github.com>
2023-03-10 13:52:50 -0300

    Update InteiroSet.java

--- aula3/ex4/InteiroSet.java
+++ aula3/ex4/InteiroSet.java
@@ -1,5 +1,17 @@
 package edu.br.marielle.aula3.ex4;
 
+/*Crie uma classe em Java chamada InteiroSet, tendo como atributo um array, de valores booleanos, de 100 posições. 
+Tal array representa os inteiros no intervalo de 0 a 99. Por exemplo: o elemento do array a[i] é true se o inteiro i estiver no conjunto. 
+O elemento do array a[j] é false se o inteiro não estiver no conjunto.
+O construtor, sem argumentos, inicializa o array com um “conjunto vazio” (todos os valores false).
+Forneça os seguintes métodos:
+- Método uniao: retorna um terceiro conjunto que é a união teórica de dois conjuntos existentes (isto é, aplicação da função lógica OU sobre os conjuntos e retorna o valor lógico true ou false);
+- Método interseccao: retorna um terceiro conjunto que é a intersecção teórica de dois conjuntos existentes (isto é, aplicação da função lógica AND sobre os conjuntos e retorna o valor lógico true ou false);
+- Método insereElemento: insere um novo elemento inteiro k em um conjunto (configurando a[k] como true);
+- Método removeElemento: que exclui o inteiro m (configurando a[m] como false).
+- Método converteEmString: retorna uma string contendo um conjunto como uma lista de números separados por espaço. Inclua somente os elementos que estão presentes no conjunto. Utilize – para representar um conjunto vazio;
+- Método eIgualA determina se dois conjuntos são iguais;*/
+
 import java.util.Arrays;
 
 public class InteiroSet {
