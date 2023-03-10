8b38ce4d5a110c0d110d102ed72bafefb52dd27d
marivaz0 <118058661+marivaz0@users.noreply.github.com>
2023-03-10 13:47:32 -0300

    Update Aluno.java

--- aula1/ex2/Aluno.java
+++ aula1/ex2/Aluno.java
@@ -1,5 +1,10 @@
 package edu.br.marielle.aula1.ex2;
 
+/*Crie uma classe Aluno com os atributos: nome, genero e idade
+Crie uma classe de testes, contendo o método main, crie dois objetos da classe Aluno e exiba no console o nome de cada um deles
+Crie um método estático que gere a idade do aluno de forma aleatória, no intervalo de 20 a 60 anos
+Crie um método estático que formate o nome do aluno, colocando o nome em letras maiúsculas*/
+
 public class Aluno {
     String nome, genero;
     int idade;
